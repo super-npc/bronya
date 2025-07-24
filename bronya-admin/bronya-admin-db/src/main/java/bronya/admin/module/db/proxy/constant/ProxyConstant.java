92cd49044e9523fafa223c08889239cf718f56b0
wenxichen <lianlianyi@vip.qq.com>
2025-07-24 09:59:50 +0800

    refactor:

--- bronya-admin/bronya-admin-db/src/main/java/bronya/admin/module/db/proxy/constant/ProxyConstant.java
+++
@@ -1,8 +0,0 @@
-package bronya.admin.module.db.proxy.constant;
-
-import bronya.admin.module.db.proxy.domain.ProxyDo;
-
-public class ProxyConstant {
-    public static final ProxyDo proxyDo = new ProxyDo(ProxyDo.ProxyType.DIRECT, "127.0.0.1", 10000);
-
-}
